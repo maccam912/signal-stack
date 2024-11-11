bbed1bf39f282c690a27604de40b6326ee0668f3
Matt Koski <maccam912@gmail.com>
2024-11-10 18:28:14 -0600

    setting up camera

--- Scenes/funnel.gd
+++ Scenes/funnel.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var cooldown: float = 0.5
+
 const scenes = [
 		preload("res://Scenes/Junk/Umbrella.tscn"),
 		preload("res://Scenes/Junk/Stopsign.tscn"),
@@ -10,6 +12,8 @@ const scenes = [
 		preload("res://Scenes/Junk/Beachball.tscn"),
 	]
 
+var last_drop_time: float = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -18,12 +22,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("Drop"):
-		# Spawn the thingy
-		# select random scene from scenes
-		var scene = scenes[randi_range(0, len(scenes)-1)]
-		var s = scene.instantiate()
-		s.position = position
-		add_sibling(s)
+		var current_time = Time.get_unix_time_from_system()
+		if current_time - last_drop_time >= cooldown:
+			# Spawn the thingy
+			var scene = scenes[randi_range(0, len(scenes)-1)]
+			var s = scene.instantiate()
+			s.position = position
+			add_sibling(s)
+			last_drop_time = current_time
 	if Input.is_action_pressed("Left"):
 		position.x -= 10
 	if Input.is_action_pressed("Right"):
